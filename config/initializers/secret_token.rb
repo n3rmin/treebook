adaad2dda04963f73f8e0df31fec5833a08eff9d
Asimov <asimov.kucevic@gmail.com>
2013-04-05 11:48:52 +0200

    First git rails appp, and added it on git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '2e4bc454b92b4b7b0b7d15d15a3d0901cb4f50bfea60990bc492649792c9d82e1529d7283aa167f4c992813dcf96b52924e746b877e2e160bfd38b6f4c64a951'
